055a51e7580b3b90984e1ede638f49336c8c7aec
charline mathus <charlinemathus@rns-jaws.local>
2019-12-04 15:24:57 +0100

    modification de la police des menus et des liens de connexion 'deja eater' et 'devenir eater'. Modification du logo unicolor bleu avec suppression de la guide line

---
+++ integration/_structure_bootsrtap_gulp/src/js/main.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import 'bootstrap'; // j'importe bootstrap.js depuis node_modules
+// import owlCarousel from 'owl.carousel'; /* si il y a un carousel */
+
+
+
+$(document).ready(function () {
+
+  
+
+});
